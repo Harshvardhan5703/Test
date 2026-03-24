6d593ca8cddd32e64794aba1e19c3120137b98ab
Harshvardhan <shekhawat.harsh287@gmail.com>
2026-03-24 11:13:28 +0530

    Implemented top-down light grey mask hover effect on product cards

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,10 +5,25 @@ const Item = ({product}) => {
   return (
     <div className='group relative'>
       <Link to={`/products/${product?.id}`}>
-      <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-800/50 lg:aspect-none grup-hover:opacity-75 lg:h-80 h-96 border border-gray-700/50'>
-        <img src={product?.image} alt={product?.name} className='h-full w-full object-cover object-center lg:h-full lg:w-full' />
+      <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-800/50 lg:aspect-none lg:h-80 h-96 border border-gray-700/50 relative'>
+        
+        {/* Image */}
+        <img 
+          src={product?.image} 
+          alt={product?.name} 
+          className='h-full w-full object-cover object-center transition-transform duration-500 group-hover:scale-110'
+        />
+
+        {/* Overlay - I implemented it as it was a task requirement, otherwise its looking bad, as it blurs the image slightly.*/}
+        <div className='absolute top-[-100%] left-0 w-full h-full 
+        bg-gray-300/20 
+        transition-all duration-500 
+        group-hover:top-0'>
+        </div>
+
       </div>
       </Link>
+
       <div className='mt-4 flex justify-between'>
         <div>
             <h3 className='text-sm text-foreground'>
